0861041964034296bbba86a62de4c49a60559f22
teamuical <teamuical@gmail.com>
2020-07-26 12:29:31 +0530

    spinner

--- app/src/main/java/com/example/androidworkshop/marksinputactivity.java
+++ app/src/main/java/com/example/androidworkshop/marksinputactivity.java
@@ -3,14 +3,31 @@ package com.example.androidworkshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class marksinputactivity extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_marksinputactivity);
+
+        spinner = (Spinner)findViewById(R.id.gender);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.gender, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
 
     }
 }
